ef2534bccf248ad206748b3ba9d167fc20340388
dcastro123 <davidcastro853@gmail.com>
2017-06-02 02:15:56 -0700

    updated main

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -12,6 +12,20 @@
 
 #include "ft_printf.h"
 
+void	init_args(t_args *ar)
+{
+	ar.space = 0;
+ 	ar.plus = 0;
+ 	ar.zero = 0;
+ 	ar.tag_x = 0;
+ 	ar.tag_X = 0;
+ 	ar.tag_o = 0;
+ 	ar.precision = 0;
+ 	ar.width = 0;
+	ar.out = ft_strnew(0);
+	ar.conv = 0;
+}
+
 void	start_conversion(const char *format, t_args *ar, int index)
 {
 	init_args(ar);
