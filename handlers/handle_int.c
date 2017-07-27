0c252fab6b5369d7d71c29ec7141f2fe7c5680f6
David CASTRO-CHEW <dcastro-@e1z2r6p5.42.us.org>
2017-07-26 18:48:09 -0700

    pass printf.com?

--- handlers/handle_int.c
+++ handlers/handle_int.c
@@ -6,51 +6,30 @@
 /*   By: dcastro- <dcastro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/21 15:34:33 by dcastro-          #+#    #+#             */
-/*   Updated: 2017/07/25 22:58:47 by dcastro-         ###   ########.fr       */
+/*   Updated: 2017/07/26 18:47:44 by dcastro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
-// static	void	format_prec(t_args *ar, char *tmp)
-// {
-// 	int j;
-// 	int i;
-// 	int len;
-
-// 	len = (int)ft_strlen(tmp);
-// 	j = -1;
-// 	i = -1;
-// 	if (ar->precision == 0 && *tmp = '0')
-// 		ar->str_out = ft_strdup('');
-// 	else if (ar->precision > len)
-// 	{
-// 		ar->str_out = ft_memalloc(ar->precision);
-// 		ar->str_out[ar->precision] = '\0';
-// 		while (++i < (ar->precision - len))
-// 			ar->str_out[i] = '0';
-// 		while (++i < ar->precision)
-// 			ar->str_out[i] = tmp[++j];
-// 	}
-// 	else
-// 		ar->str_out = ft_strdup(tmp);
-// }
 
 void	handle_int(t_args *ar)
 {
 	char *tmp;
 
 	tmp = ft_itoa(ar->num);
-//	format_prec(ar, tmp);
+	format_prec(ar, tmp);
+	// printf("string after precision formatting: '%s'\n", ar->str_out);
 	if (ar->zero == 1 && ar->minus == 0 && ar->pflag == 0)
 		format_intpadding(ar, tmp);
 	else if (ar->minus == 1)
 		format_intljust(ar, tmp);
-	else
+	else if (ar->width > 0)
 	{
-		format_intwidth(ar, tmp);
+		format_intwidth(ar);
 		ft_putstr(ar->str_out);
 	}
+	else
+		ft_putstr(ar->str_out);
 }
 
 void	handle_usint(t_args *ar)
@@ -58,17 +37,16 @@ void	handle_usint(t_args *ar)
 	char *tmp;
 
 	tmp = ft_uitoa(ar->unum);
-//	format_prec(ar, tmp);
+	format_prec(ar, tmp);
 	if (ar->zero == 1 && ar->minus == 0 && ar->pflag == 0)
 		format_intpadding(ar, tmp);
 	else if (ar->minus == 1)
 		format_intljust(ar, tmp);
 	else if (ar->width > 0)
 	{
-		format_intwidth(ar, tmp);
+		format_intwidth(ar);
 		ft_putstr(ar->str_out);
 	}
 	else
 		ft_putstr(tmp);
-	ft_putstr(tmp);
 }
--- helpers/format_int.c
+++ helpers/format_int.c
@@ -6,11 +6,39 @@
 /*   By: dcastro- <dcastro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/02 20:56:27 by dcastro-          #+#    #+#             */
-/*   Updated: 2017/07/25 23:11:34 by dcastro-         ###   ########.fr       */
+/*   Updated: 2017/07/26 18:46:28 by dcastro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+void	format_prec(t_args *ar, char *tmp)
+{
+	int j;
+	int i;
+	int len;
+
+	len = (int)ft_strlen(tmp);
+	j = 0;
+	i = -1;
+	if (ar->precision == 0 && *tmp == '0')
+		return ;
+	else if (ar->precision > len)
+	{
+		ar->str_out = ft_memalloc(ar->precision);
+		ar->str_out[ar->precision] = '\0';
+		while (++i < (ar->precision - len))
+			ar->str_out[i] = '0';
+		while (i < ar->precision)
+		{
+			ar->str_out[i] = tmp[j];
+			j++;
+			i++;
+		}
+	}
+	else
+		ar->str_out = ft_strdup(tmp);
+}
 
 void	format_intljust(t_args *ar, char *tmp)
 {
@@ -23,12 +51,10 @@ void	format_intljust(t_args *ar, char *tmp)
 	{
 		ft_putchar('+');
 		ar->ret++;
+		i++;
 	}
 	while (ar->str_out[++i])
-	{
 		ft_putchar(ar->str_out[i]);
-		ar->ret++;
-	}
 	if (ar->width > 0)
 	{
 		i = -1;
@@ -40,6 +66,7 @@ void	format_intljust(t_args *ar, char *tmp)
 			ar->ret++;
 		}
 	}
+	ar->ret += ft_strlen(ar->str_out);
 }
 
 void	format_intpadding(t_args *ar, char *tmp)
@@ -52,41 +79,47 @@ void	format_intpadding(t_args *ar, char *tmp)
 	{
 		ft_putchar('+');
 		ar->ret++;
+		i++;
 	}
 	if (ar->width > 0)
 	{
 		while (++i < (ar->width - len))
 		{
-			ar->str_out[i] = '0';
+			ft_putchar('0');
 			ar->ret++;
 		}
 	}
+	ft_putstr(ar->str_out);
 	ar->ret += ft_strlen(ar->str_out);
 }
 
-void	format_intwidth(t_args *ar, char *tmp)
+void	format_intwidth(t_args *ar)
 {
 	int	i;
-	int	j;
+	int len;
 
-	i = 0;
-	j = 0;
-	int len = (int)ft_strlen(tmp);
-	ar->str_out = ft_memalloc(ar->width);
+	i = -1;
+	len = (int)ft_strlen(ar->str_out);
+	if (len > ar->width)
+	{
+		ar->ret += len;
+		return ;
+	}
 	if (ar->plus == 1 && ar->num >= 0)
+	{
+		ar->ret++;
+		i++;
 		ft_putchar('+');
-	while (i < ar->width - len)
-	{
-		ar->str_out[i] = ' ';
-		i++;
 	}
-	while (i < ar->width)
+	if (ar->width > ar->precision)
 	{
-		ar->str_out[i] = tmp[j];
-		j++;
-		i++;
+		while (++i < ar->width - len)
+		{
+			ft_putchar(' ');
+			ar->ret++;
+			// i++;
+		}
 	}
-	ar->str_out[i] = '\0';
-	ar->ret += ft_strlen(ar->str_out) > 0 ? ft_strlen(ar->str_out) : ft_strlen(tmp);
+	ar->ret += len;
 }
 
